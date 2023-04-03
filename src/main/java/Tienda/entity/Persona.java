83caa71595a44be2004c0b477b32bce0ab0245d1
Darielk51 <94978506+Darielk51@users.noreply.github.com>
2023-04-03 12:28:40 -0600

    Faltan arreglar errores

--- src/main/java/Tienda/entity/Persona.java
+++ src/main/java/Tienda/entity/Persona.java
@@ -4,15 +4,18 @@
  */
 package Tienda.entity;
 
-import static com.fasterxml.jackson.databind.util.ClassUtil.name;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "personas")
@@ -22,15 +25,52 @@ public class Persona implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    private String nombre; 
-    private String apellido; 
-    private String apellido2; 
-    private String telefono; 
-    private String gmail; 
-    
+    private String nombre;
+    private String apellido;
+    private String apellido2;
+    private String telefono;
+    private String gmail;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    private String password;
+    private int active;
+    private String roles = "";
+    private String permission = "";
+
     @ManyToOne
-    @JoinColumn(name="paises_id")
-    private Pais pais; 
+    @JoinColumn(name = "paises_id")
+    private Pais pais;
 
     public long getId() {
         return id;
@@ -87,5 +127,20 @@ public class Persona implements Serializable {
     public void setPais(Pais pais) {
         this.pais = pais;
     }
-    
+
+    public List<String> getRoleList() {
+        if (this.roles.length() > 0) {
+
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+
+        public List<String> getPermissionList() {
+        if (this.permission.length() > 0) {
+
+            return Arrays.asList(this.permission.split(","));
+        }
+        return new ArrayList<>();
+    }
 }
---
+++ src/main/java/Tienda/service/Userprincipal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tienda.service;
+
+/**
+ *
+ * @author jvlan
+ */
+public class Userprincipal implements UserDetails{
+    private Persona = persona; 
+    
+    public Userprincipal (Persona persona){
+    this.persona = persona;
+    }
+    
+    
+    @override
+    public Collection<? extends GrantedAuthority > get Autorities (){
+    List<GrantedAutoty> authority = new SimpleGrantedAuthority(p);}
+}
